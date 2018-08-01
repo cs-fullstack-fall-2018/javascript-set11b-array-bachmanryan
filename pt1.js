47709633bf7c2e83423cbd9112f0064cba52597b
Ryan Bachman <jacksprat455@gmail.com>
2018-08-01 14:41:19 -0500

    Array_Classwork

---
+++ pt1.js
@@ -0,0 +1,29 @@
+// function main(){
+//     var myArray = [];
+//     myArray.push("bob");
+//     myArray.push("dave");
+//     myArray.push("greg");
+//     myArray.push("red");
+//     console.log(myArray);
+// }
+// main();
+
+
+
+
+
+
+// function main() {
+//     var myArray = [4,1,7,3,5];
+//     console.log(myArray);
+// }
+// main();
+
+
+function main() {
+    var myArray = ["bob", "greg", "dave", "Kenn"];
+    console.log(myArray[2]);
+    myArray.splice(1, 1);
+    console.log(myArray[2]);
+}
+main();
